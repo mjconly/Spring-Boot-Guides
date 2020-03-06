967baa1c93b946fffd3d2217cc182f49ba4faad6
Mike <mjconly@gmail.com>
2020-03-06 00:54:49 -0600

    initial consume service

---
+++ ConsumeService/src/main/java/com/example/ConsumeService/ConsumeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConsumeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeServiceApplication.class, args);
+	}
+
+}
--- RESTfulWebService/src/main/java/com/example/RESTfulWebService/GreetingController.java
+++ RESTfulWebService/src/main/java/com/example/RESTfulWebService/GreetingController.java
@@ -8,11 +8,11 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    private static final String template = "Hello, %s";
+    private static final String template = "Hello, %s, %d";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name){
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(template, name, counter.incrementAndGet()));
     }
 }
